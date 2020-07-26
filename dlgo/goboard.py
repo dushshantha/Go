b143dc36408ac69e46db9c30012840292903a235
dushshantha <dushshantha.chandaradasa@gmail.com>
2020-07-25 18:03:16 -0700

    Added faster make bot and Human vs Bot game

--- dlgo/goboard.py
+++ dlgo/goboard.py
@@ -163,6 +163,11 @@ class GameState():
         self.board = board
         self.next_player = next_player
         self.previous_state = previous
+        if self.previous_state is None:
+            self.previous_state = frozenset()
+        else:
+            self.previous_state = frozenset(
+                previous.previous_state | {(previous.next_player, previous.board.zobrist_hash())})
         self.last_move = move
 
     '''
@@ -211,14 +216,9 @@ class GameState():
             return False
         next_board = copy.deepcopy(self.board)
         next_board.place_stone(player, move.point)
-        next_situation = (player.other, next_board)
-        past_state = self.previous_state
-        while past_state is not None:
-            if past_state.situation == next_situation:
-                return True
-            past_state = past_state.previous_state
+        next_situation = (player.other, next_board.zobrist_hash())
+        return next_situation in self.previous_state
 
-        return False
 
     def is_valid_move(self, move):
         if self.is_over():
